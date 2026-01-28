ac7f48de006b528d417eb3cd4a24e80e500da185
ALmohawis <f14commander@gmail.com>
2026-01-28 21:20:27 +0300

    Add Install Repository Key to debian-apt & ubuntu-apt

--- debian-apt.sh
+++ debian-apt.sh
@@ -9,12 +9,19 @@ Debian 12 [2]
 read -p "==>" op
 
 if [[ "$op" == "1" ]]; then
-	echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 11 main"| \
+    curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
+   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
+   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
+
+    echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 11 main"| \
    tee /etc/apt/sources.list.d/zammad.list > /dev/null
    sudo apt update
    sudo apt install zammad -y
 elif [[ "$op" == "2" ]]; then
-	echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 12 main"| \
+    curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
+   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
+   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 12 main"| \
    tee /etc/apt/sources.list.d/zammad.list > /dev/null
    sudo apt update
    sudo apt install zammad -y
--- ubuntu-apt.sh
+++ ubuntu-apt.sh
@@ -8,21 +8,27 @@ Ubuntu 24.04  [3]
 "
 
 read -p "==>" op
-curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
-   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
-   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
 
 if [[ "$op" == "1" ]]; then
+    curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
+   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
+   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
 	echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 20.04 main"| \
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null
     sudo apt update
    sudo apt install zammad -y
 elif [[ "$op" == "2" ]]; then
+   curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
+   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
+   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
    echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 22.04 main"| \
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null	
    sudo apt update
    sudo apt install zammad -y
 elif [[ "$op" == "3" ]]; then
+   curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
+   gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
+   && sudo chmod 644 /etc/apt/keyrings/pkgr-zammad.gpg
    echo "deb [signed-by=/etc/apt/keyrings/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 24.04 main"| \
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null
    sudo apt update
