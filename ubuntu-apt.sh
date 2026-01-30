67fec4cb4bb814939f3f2f40b239f94f50790d70
ALmohawis <f14commander@gmail.com>
2026-01-30 18:25:48 +0300

    add run elasticsearch and zammad status to ubuntu

--- ubuntu-apt.sh
+++ ubuntu-apt.sh
@@ -17,6 +17,12 @@ if [[ "$op" == "1" ]]; then
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null
     sudo apt update
    sudo apt install zammad -y
+   zammad run rails r "Setting.set('es_url', 'http://localhost:9200')"
+   curl http://localhost:9200
+   echo "If The Output is \"Failed to connect\" Please Run \" systemctl status elasticsearch.service \" \"sudo systemctl start elasticsearch.service \""
+   echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+   systemctl status zammad
+   echo "If zammad Is Active , Install Done"
 elif [[ "$op" == "2" ]]; then
    curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
    gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
@@ -25,6 +31,11 @@ elif [[ "$op" == "2" ]]; then
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null	
    sudo apt update
    sudo apt install zammad -y
+   zammad run rails r "Setting.set('es_url', 'http://localhost:9200')"
+   echo "If The Output is \"Failed to connect\" Please Run \" systemctl status elasticsearch.service \" \"sudo systemctl start elasticsearch.service \""
+   echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+   systemctl status zammad
+   echo "If zammad Is Active , Install Done"
 elif [[ "$op" == "3" ]]; then
    curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
    gpg --dearmor | sudo tee /etc/apt/keyrings/pkgr-zammad.gpg> /dev/null \
@@ -33,6 +44,11 @@ elif [[ "$op" == "3" ]]; then
    sudo tee /etc/apt/sources.list.d/zammad.list > /dev/null
    sudo apt update
    sudo apt install zammad -y
+   zammad run rails r "Setting.set('es_url', 'http://localhost:9200')"
+   echo "If The Output is \"Failed to connect\" Please Run \" systemctl status elasticsearch.service \" \"sudo systemctl start elasticsearch.service \""
+   echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+   systemctl status zammad
+   echo "If zammad Is Active , Install Done"
 else
     echo "Error"
     exit 0
