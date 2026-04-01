7fbd5ecc1389063deed54ba94141eebf1b0de92c
ALmohawis <sasm.contact@gmail.com>
2026-04-01 19:48:39 +0300

    Check If The User Install docker compose Or No Check If The Command Is docker-compose Or docker compose

--- func/docker.sh
+++ func/docker.sh
@@ -37,6 +37,18 @@
 # }
 
 DockerDownload() {
+
+# Is There docker compose ? & What Is The Virson ?
+if command -v docker-compose &> /dev/null; then
+    DC="docker-compose"
+elif docker compose version &> /dev/null; then
+    DC="docker compose"
+else
+    echo "Docker Compose not installed!"
+    return 1
+fi
+
+
 git clone https://github.com/zammad/zammad-docker-compose.git
 cd zammad-docker-compose
 echo "Zammad Port? (http=80 But Check If There Any Service Using This Port) :"
@@ -44,5 +56,5 @@ read -p "===>" Port
 cp .env.dist .env
 echo "NGINX_EXPOSE_PORT=$Port
 NGINX_PORT=8080" | tee -a .env
-docker compose up -d
+$DC up -d
 }
